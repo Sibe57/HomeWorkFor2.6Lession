0ca068f6b06ff85466585c7210e83a1385df6144
Fedor Eronin <eroninf@icloud.com>
2022-07-02 15:43:51 +0300

    Initial Commit

---
+++ HomeWorkFor2.6Lession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkFor2.6Lession
+//
+//  Created by Федор Еронин on 02.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
