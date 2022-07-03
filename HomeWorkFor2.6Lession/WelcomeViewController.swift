3099958421757dd3dc79fdad41c4c4a18aa37a2b
Fedor Eronin <eroninf@icloud.com>
2022-07-03 10:43:06 +0300

    add welcomeViewController set segues, and UI

---
+++ HomeWorkFor2.6Lession/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  HomeWorkFor2.6Lession
+//
+//  Created by Федор Еронин on 03.07.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    var userName: String!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGragient()
+        welcomeLabel.text = "Welcome, \(userName ?? "User")"
+    }
+    
+    private func setGragient() {
+        let gradient = CAGradientLayer()
+        let colorTop = UIColor(displayP3Red: 0.5, green: 0, blue: 0.5, alpha: 0.6)
+        let colorBottom = UIColor(displayP3Red: 0, green: 1, blue: 1, alpha: 0.5)
+        gradient.frame = view.bounds
+        gradient.colors = [colorTop.cgColor, colorBottom.cgColor]
+        view.layer.insertSublayer(gradient, at: 0)
+    }
+}
