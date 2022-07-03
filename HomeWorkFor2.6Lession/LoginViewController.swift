a6ec095b268673711e6c45fc9b62c5215ba2967a
Fedor Eronin <eroninf@icloud.com>
2022-07-03 10:44:37 +0300

    add keyboard show/hide functions, set segues

--- HomeWorkFor2.6Lession/LoginViewController.swift
+++ HomeWorkFor2.6Lession/LoginViewController.swift
@@ -20,7 +20,31 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        userNameTF.delegate = self
+        passwordTF.delegate = self
+        setObservers()
         setButtonsLabels()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func setObservers() {
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
     
     private func setButtonsLabels() {
@@ -65,7 +89,7 @@ class LoginViewController: UIViewController {
             return
         }
         if userNameInput == userName && passwordInput == password {
-            
+            performSegue(withIdentifier: "toWelcomeScreen", sender: self)
         } else {
             showDefaultAllert(title: "Invalid login or password",
                               message: "Please enter correct login and password",
@@ -74,7 +98,40 @@ class LoginViewController: UIViewController {
         }
     }
     
+    //MARK: - Navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeViewController = segue.destination as?
+                WelcomeViewController else { return }
+        
+        welcomeViewController.userName = userNameTF.text
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+}
+
+//MARK: - UITextFieldDelegate
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if self.view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= 100
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 }
 
